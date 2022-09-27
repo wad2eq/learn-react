1182ed4177a2a5f7bb206c78cc246c7aed7ce9fa
wad2eq <mirekkedzierski@gmail.com>
2022-09-27 15:29:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# learn-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-3sg7ya)
---
+++ src/home/HomePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-ruter-dom';
+
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
